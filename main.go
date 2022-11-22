8b420f2154f2b60803f95ef3e0e628d174ccbcfe
unknown <andianugrahaqsa@gmail.com>
2022-11-22 14:11:24 +0800

    cicd99

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 func sayHallo(c echo.Context) error {
 	return c.JSON(200, map[string]any{
-		"message":  "hallo dunia tipu2 aaaaaaa",
+		"message":  "hallo dunia tipu2 aaaaaaa alhamdulillah ya Allah",
 		"app name": utils.GetConfig("APP_NAME"),
 		"db name":  utils.GetConfig("DB_NAME"),
 	})
